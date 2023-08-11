153e16e7bd3f73a388aab1e9903fcf4f1d3c8cd2
Emon <emon.mhk69@gmail.com>
2023-08-12 00:20:09 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-semibold text-center'>
+          Learning Redux Toolkit
+      </h1>
+    </>
+  )
+}
+
+export default App
